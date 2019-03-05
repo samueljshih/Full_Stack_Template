34cbde9694b92580489381d03df32f76840c02eb
Samuel Shih <samuels.shih@gmail.com>
2019-03-05 11:50:33 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
+// const sql = require('../db/databaseReqHandler.js');
+
+app.set('port', 3000);
+
+app.use(express.static('public'));
+app.use(parser.json());
+
+app.get('/', (request, response) => {
+  response.end();
+});
+
+app.post('/', (request, response) => {
+  //   sql.insertUser('Sam').then(result => {
+  //     sql.getUsers().then(result => {
+  //       console.log(result);
+  //       response.end();
+  //     });
+  //   });
+});
+
+app.listen(app.get('port'));
+console.log('listening on port 3000....');
